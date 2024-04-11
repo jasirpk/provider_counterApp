416b3a0bcc405a30ddf49b6bb04e857cd9774602
jasir <jasijasu959@gmail.com>
2024-04-11 15:39:11 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,75 @@
+// ignore_for_file: avoid_print
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:state_provider/counter_notifier.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<CounterNotifier>(
+            create: (_) => CounterNotifier())
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    final counterData = Provider.of<CounterNotifier>(context, listen: false);
+    print('runnning MyApp ');
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Consumer<CounterNotifier>(builder: (context, data, _) {
+              return Text(
+                counterData.counter.toString(),
+                style: Theme.of(context).textTheme.headlineMedium,
+              );
+            })
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: counterData.increment,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
